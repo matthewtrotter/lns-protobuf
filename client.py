dc8e8d4b5a6cd25d0b23a168cb3f4b31c10c0d09
Matthew Trotter <matthew.trotter1@gmail.com>
2021-05-05 09:14:36 -0400

    handle unknown alert

--- client.py
+++ client.py
@@ -1,4 +1,4 @@
-import lnsalerts2_pb2
+import lnsalerts_pb2
 from google.protobuf.any_pb2 import Any
 import zmq
 
@@ -20,34 +20,36 @@ while True:
     alert_bin = socket.recv()
 
     # Parse into Alert type
-    alert = lnsalerts2_pb2.Alert()
+    alert = lnsalerts_pb2.Alert()
     alert.ParseFromString(alert_bin)
 
     print("RECEIVED:")
     print(alert)
-    print(alert_bin)
-    print()
     
     # Parse the details
-    details_bin = Any()
-    details_bin.CopyFrom(alert.details)
-    try:
-        if details_bin.Is(lnsalerts2_pb2.Alert.DESCRIPTOR):
-            details = lnsalerts2_pb2.Alert()
-        elif details_bin.Is(lnsalerts2_pb2.CertificateExpiredDetails.DESCRIPTOR):
-            details = lnsalerts2_pb2.CertificateExpiredDetails()
-        elif details_bin.Is(lnsalerts2_pb2.CertificateAlmostExpiredDetails.DESCRIPTOR):
-            details = lnsalerts2_pb2.CertificateAlmostExpiredDetails()
-        else:
-            raise ValueError(f'Unknown alert type: {details_bin}')
-        details_bin.Unpack(details)
-    except ValueError as exc:
-        print(exc)
-        details = "Unknown - could not parse details"
-    finally:
-        print('Details:')
-        print(details)
-        print()
+    if alert.HasField('details'):
+        details_bin = Any()
+        details_bin.CopyFrom(alert.details)
+        try:
+            if details_bin.Is(lnsalerts_pb2.Alert.DESCRIPTOR):
+                details = lnsalerts_pb2.Alert()
+            elif details_bin.Is(lnsalerts_pb2.CertificateExpiredDetails.DESCRIPTOR):
+                details = lnsalerts_pb2.CertificateExpiredDetails()
+            elif details_bin.Is(lnsalerts_pb2.CertificateAlmostExpiredDetails.DESCRIPTOR):
+                details = lnsalerts_pb2.CertificateAlmostExpiredDetails()
+            else:
+                raise ValueError(f'Unknown alert type: {details_bin}')
+            details_bin.Unpack(details)
+        except ValueError as exc:
+            print(exc)
+            details = "Unknown - could not parse details"
+        finally:
+            print('Details:')
+            print(details)
+            print()
+    else:
+        print('No Details')
 
+    print()
 
 
--- lns.py
+++ lns.py
@@ -19,7 +19,7 @@ details2 = lnsalerts2_pb2.CertificateExpiredDetails()
 details2.pubkey = "ssh-ed25519 AAAAC3NzaC..."
 details2.expirationtime = time.time() - 10000
 details2_packed = Any()
-details2_packed.Pack(details2)
+details2_packed.Pack(details2)  # Pack so that we can copy into the alert
 alert2 = lnsalerts2_pb2.Alert()
 alert2.ownerid = 0
 alert2.code = 2000
@@ -27,6 +27,20 @@ alert2.subcode = 21
 alert2.message = f"Certificate X expired at time {details2.expirationtime}"
 alert2.details.CopyFrom(details2_packed)
 alert2_bin = alert2.SerializeToString()
+
+# Mux not responding alert
+details3 = lnsalerts2_pb2.MuxsNotRespondingDetails()
+details3.muxsid = "::-0"
+details3_packed = Any()
+details3_packed.Pack(details3)  # Pack so that we can copy into the alert
+alert3 = lnsalerts2_pb2.Alert()
+alert3.ownerid = 0
+alert3.code = 3000
+alert3.subcode = 6
+alert3.message = f"Muxs {details3.muxsid} not responding."
+alert3.details.CopyFrom(details3_packed)
+alert3_bin = alert3.SerializeToString()
+
 
 # Create publish socket
 context = zmq.Context()
@@ -38,12 +52,15 @@ time.sleep(1)
 socket.send(alert1_bin)
 print("SENT:")
 print(alert1)
-print(alert1_bin)
-print()
+[print() for i in range(2)]
 
 socket.send(alert2_bin)
 print("SENT:")
 print(alert2)
-print(alert2_bin)
-print()
+[print() for i in range(6)]
+
+socket.send(alert3_bin)
+print("SENT:")
+print(alert3)
+[print() for i in range(8)]
 
